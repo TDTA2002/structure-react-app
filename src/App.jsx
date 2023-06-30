0eebd6a6c6b18aab81ae26f933f3c436c8f6d737
TDTA2002 <tranduytuananh203@gmail.com>
2023-06-30 17:09:59 +0700

    add router dom,lazyload,alias

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,20 @@
 import './App.scss';
+import { Route, Routes } from 'react-router-dom';
+import LazyLoad from './LazyLoad';
+import Navbar from '@components/Navbars/Navbar';
 
 function App() {
   return (
     <div className="App">
-      123123131
+      <div className='app-container'>
+        <Navbar />
+        <Routes>
+          <Route path="" element={LazyLoad(() => import("@pages/Homes/Home"))()} />
+          <Route path="/about" element={LazyLoad(() => import("@pages/Abouts/About"))()}>
+            <Route path='my-infor' element={LazyLoad(() => import("@pages/Abouts/MyInFors/MyInFor"))()}></Route>
+          </Route>
+        </Routes>
+      </div>
     </div>
   );
 }
